51ad00f9416dcedc7e143d3bbc695d155b5f5237
MAlabdulhadi <65816656+MAlabdulhadi@users.noreply.github.com>
2023-05-23 14:18:19 +0300

    homework18

--- homework18/src/main/java/com/example/homework17/Repository/UserRepository.java
+++ homework18/src/main/java/com/example/homework17/Repository/UserRepository.java
@@ -15,10 +15,14 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT user from User user where user.userName=?1 and user.password=?2 ")
     User singIn(String userName, String password);
 
+    //@Query("SELECT user from User user where user.email=?1")
     User getUserByEmail(String email);
 
+    //@Query("SELECT user from User user where user.role=?1")
     List<User> getAllUserByRole(String role);
 
+    //@Query("SELECT user from User user where user.email=?1")
     List<User> getAllUserByAgeGreaterThanEqual(int age);
+
 
 }
