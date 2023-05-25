1cade3306950dc84896150718f0ba15d673d4fc6
MAlabdulhadi <65816656+MAlabdulhadi@users.noreply.github.com>
2023-05-25 14:32:55 +0300

    Exercises

--- Exercises/src/main/java/com/example/exercises/ControllerAdvise/ControllerAdvise.java
+++ Exercises/src/main/java/com/example/exercises/ControllerAdvise/ControllerAdvise.java
@@ -2,11 +2,16 @@ package com.example.exercises.ControllerAdvise;
 
 import com.example.exercises.ApiException.ApiException;
 import com.example.exercises.ApiResponse.ApiResponse;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @ControllerAdvice
 public class ControllerAdvise {
@@ -22,10 +27,36 @@ public class ControllerAdvise {
         return ResponseEntity.status(400).body(new ApiResponse(message));
     }
 
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity<ApiResponse> ConstraintViolationException(ConstraintViolationException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(new ApiResponse(message));
+    }
+
+    @ExceptionHandler(value = SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<ApiResponse> SQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(new ApiResponse(message));
+    }
+
+    @ExceptionHandler(value = InvalidDataAccessResourceUsageException.class)
+    public ResponseEntity<ApiResponse> InvalidDataAccessResourceUsageException(InvalidDataAccessResourceUsageException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(new ApiResponse(message));
+    }
+
     @ExceptionHandler(value = DataIntegrityViolationException.class)
     public ResponseEntity<ApiResponse> DataIntegrityViolationException(DataIntegrityViolationException e) {
         String message = e.getMessage();
         return ResponseEntity.status(400).body(new ApiResponse(message));
     }
+
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse> HttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(new ApiResponse(message));
+    }
+
 
 }
